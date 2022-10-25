e0eca005d92eae4c58d7d7e639999a1a5e8a883a
pcClase <ikadh@plaiaundi.net>
2022-10-25 12:40:17 +0200

    first commit

---
+++ app/src/main/java/com/example/crunchyandnetflix/ui/catalogue/CatalogueFragment.java
@@ -0,0 +1,37 @@
+package com.example.crunchyandnetflix.ui.catalogue;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.crunchyandnetflix.databinding.FragmentCatalogueBinding;
+
+public class CatalogueFragment extends Fragment {
+
+    private FragmentCatalogueBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        CatalogueViewModel notificationsViewModel =
+                new ViewModelProvider(this).get(CatalogueViewModel.class);
+
+        binding = FragmentCatalogueBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textNotifications;
+        notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}

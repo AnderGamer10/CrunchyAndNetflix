60b505dc81357292bc597cac92cc905ee5b8835a
pcClase <ikadh@plaiaundi.net>
2022-11-09 10:00:37 +0100

    asyntask terminado

--- app/src/main/java/com/example/crunchyandnetflix/IntroActivity.java
+++ app/src/main/java/com/example/crunchyandnetflix/IntroActivity.java
@@ -3,26 +3,114 @@ package com.example.crunchyandnetflix;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.crunchyandnetflix.Modelos.Data;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class IntroActivity extends AppCompatActivity {
-
-    Button button;
+    public static ArrayList<Data> listaCompleta = new ArrayList<>();
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
-        button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        progressBar = findViewById(R.id.dataCarga);
+
+        new Task1().execute();
+    }
+
+    class Task1 extends AsyncTask<String, Void, String>{
+        @Override
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+            getListaCompleta();
+        }
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                Thread.sleep(5000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String s) {
+            progressBar.setVisibility(View.INVISIBLE);
+            Intent newActivity = new Intent(IntroActivity.this, MainActivity.class);
+            IntroActivity.this.startActivity(newActivity);
+
+        }
+    }
+    public ArrayList<Data> getListaCompleta() {
+        String url = "https://api.tvmaze.com/shows";
+        StringRequest postResquest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
-            public void onClick(View view) {
-                Intent newActivity = new Intent(IntroActivity.this, MainActivity.class);
-                IntroActivity.this.startActivity(newActivity);
+            public void onResponse(String response) {
+                try {
+                    JSONArray jsonObject = new JSONArray(response);
+
+                    for (int i = 0; i < jsonObject.length(); i++) {
+                        JSONObject j = jsonObject.getJSONObject(i);
+                        listaCompleta.add(new Data(j.getInt("id"),j.getString("name"),j.getJSONArray("genres"),j.getJSONObject("rating").getString("average"),j.getJSONObject("image").getString("medium"),j.getJSONObject("image").getString("original"),j.getString("summary")));
+
+
+//                        TODO: Codigo para obtener los generos(separados) de cada serie
+//                        ArrayList<String> exampleList = new ArrayList<String>();
+//                        JSONArray genre = j.getJSONArray("genres");
+//
+//                        for (int a = 0; a < genre.length(); a++) {
+//                            exampleList.add((String) genre.get(a));
+//                        }
+//                        int size = exampleList.size();
+//                        String[] stringArray = exampleList.toArray(new String[size]);
+//
+//                        Log.i("asdfasdass","Output String array will be : ");
+//                        for (String s : stringArray) {
+//                            Log.i("saaa",s);
+//                        }
+
+                    }
+
+//                    TODO: Codigo para obtener las series con mayor valoracion
+//                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//                        listaCompleta.removeIf(o -> Objects.equals(o.getRating(), "null"));
+//                        listaCompleta.sort((o1,o2)->o1.getRating().compareTo(o2.getRating()));
+//                    }
+//                    Collections.reverse(listaCompleta);
+//                    for (int i = 0; i < 10; i++) {
+//                        Log.i("s", String.valueOf(listaCompleta.get(i).getRating()));
+//                        Log.i("s", String.valueOf(listaCompleta.get(i).getGenres()));
+//                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Error", error.getMessage());
             }
         });
+        Volley.newRequestQueue(this).add(postResquest);
 
-
+        return listaCompleta;
     }
 }
--- app/src/main/java/com/example/crunchyandnetflix/MainActivity.java
+++ app/src/main/java/com/example/crunchyandnetflix/MainActivity.java
@@ -31,13 +31,17 @@ import java.util.Iterator;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-    public static ArrayList<Data> listaCompleta = new ArrayList<>();
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getListaCompleta();
+
+//        TODO: La carga de la api se hace en el activity Intro(antes de iniciar el activity main)
+        Log.i("adasdasdasd", String.valueOf(IntroActivity.listaCompleta.size()));
+        for (Data data : IntroActivity.listaCompleta) {
+            Log.i("adsasd", String.valueOf(data.getRating()));
+        }
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -51,62 +55,5 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-    }
-
-
-    public ArrayList<Data> getListaCompleta() {
-        String url = "https://api.tvmaze.com/shows";
-        StringRequest postResquest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                try {
-                    JSONArray jsonObject = new JSONArray(response);
-
-                    for (int i = 0; i < jsonObject.length(); i++) {
-                        JSONObject j = jsonObject.getJSONObject(i);
-                        listaCompleta.add(new Data(j.getInt("id"),j.getString("name"),j.getJSONArray("genres"),j.getJSONObject("rating").getString("average"),j.getJSONObject("image").getString("medium"),j.getJSONObject("image").getString("original"),j.getString("summary")));
-
-
-//                        TODO: Codigo para obtener los generos(separados) de cada serie
-//                        ArrayList<String> exampleList = new ArrayList<String>();
-//                        JSONArray genre = j.getJSONArray("genres");
-//
-//                        for (int a = 0; a < genre.length(); a++) {
-//                            exampleList.add((String) genre.get(a));
-//                        }
-//                        int size = exampleList.size();
-//                        String[] stringArray = exampleList.toArray(new String[size]);
-//
-//                        Log.i("asdfasdass","Output String array will be : ");
-//                        for (String s : stringArray) {
-//                            Log.i("saaa",s);
-//                        }
-
-                    }
-
-//                    TODO: Codigo para obtener las series con mayor valoracion
-//                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-//                        listaCompleta.removeIf(o -> Objects.equals(o.getRating(), "null"));
-//                        listaCompleta.sort((o1,o2)->o1.getRating().compareTo(o2.getRating()));
-//                    }
-//                    Collections.reverse(listaCompleta);
-//                    for (int i = 0; i < 10; i++) {
-//                        Log.i("s", String.valueOf(listaCompleta.get(i).getRating()));
-//                        Log.i("s", String.valueOf(listaCompleta.get(i).getGenres()));
-//                    }
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("Error", error.getMessage());
-            }
-        });
-        Volley.newRequestQueue(this).add(postResquest);
-
-        return listaCompleta;
     }
 }
